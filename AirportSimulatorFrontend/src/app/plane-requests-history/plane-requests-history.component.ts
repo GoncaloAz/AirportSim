8352b02e176e432a72fbb49b9cac5003d7c51eb5
Goncalo Azevedo <goncalomcazevedo@gmail.com>
2021-04-01 17:15:40 +0100

    History of requests implemented

--- AirportSimulatorFrontend/src/app/plane-requests-history/plane-requests-history.component.ts
+++ AirportSimulatorFrontend/src/app/plane-requests-history/plane-requests-history.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Request } from '../shared/Request'
 
 @Component({
   selector: 'app-plane-requests-history',
@@ -8,6 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class PlaneRequestsHistoryComponent implements OnInit {
 
   constructor() { }
+
+  requests: Request[] = [
+
+    {id: 1, flightNumber : "F505", requestType: "Land", timeOfRequest: new Date(2021,4,1,17,5,0)},
+    {id: 2, flightNumber : "F504", requestType: "Departure", timeOfRequest: new Date(2021,4,1,17,10,55)},
+    {id: 1, flightNumber : "H505", requestType: "Land", timeOfRequest: new Date(2021,4,1,17,11,0)},
+    {id: 1, flightNumber : "TP505", requestType: "Land", timeOfRequest: new Date(2021,4,1,17,15,33)},
+
+  ]
 
   ngOnInit(): void {
   }
---
+++ AirportSimulatorFrontend/src/app/shared/Request.ts
@@ -0,0 +1,8 @@
+export interface Request{
+
+    id: number;
+    flightNumber: string;
+    requestType:string;
+    timeOfRequest:Date;
+
+}
